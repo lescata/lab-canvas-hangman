9b8d8df82f3deb3a64ee0f406e25cd1f8ced9334
lescata <alexandre.lescat@gmail.com>
2022-05-23 21:41:37 +0200

    first

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,31 +1,49 @@
+
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord(); // sw = resultat de picword
+    this.letters= [];  // est un tableau 
+    this.guessedLetters= "";  //est une chaine de character
+    this.errorLeft=10;   // pv debut jeux 
   }
 
   pickWord() {
-    // ... your code goes here
+    const random = Math.floor(Math.random() * this.words.length);
+    return this.words[random] //on arrondie inferieur le randomm entre 0 et longeur max du tableau
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if(keyCode > 64 && keyCode < 91){
+      return true
+    } else {
+    return false// si la key est entre 65 'a' et 90'z' vrais, si non faux
+   }
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (this.letters.includes(letter)){
+      return false
+    }else {
+      return true//si cette lettre déjà clike, faux, si non vrais)
+    }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+  this.guessedLetters = letter //
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.errorLeft -= 1;
+    this.guessedLetters.push(letter);
   }
 
   checkGameOver() {
-    // ... your code goes here
+  if (this.errorLeft <= 0){
+    return false;
+  }else{
+    return true;
+  }
   }
 
   checkWinner() {
